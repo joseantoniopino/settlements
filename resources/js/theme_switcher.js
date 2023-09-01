ec1c079d726fa2885fd05d9081fc8e7e378492b8
JoseA <josanpino@gmail.com>
2023-09-01 13:44:02 +0200

    feat(layouts): Change switcher theme button

--- resources/js/theme_switcher.js
+++ resources/js/theme_switcher.js
@@ -1,40 +1,31 @@
-let themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-let themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+// Obtén una referencia al elemento checkbox
+const themeToggleCheckbox = document.getElementById('theme-toggle'); // Reemplaza 'tuCheckboxId' con el ID de tu elemento checkbox
 
-// Change the icons inside the button based on previous settings
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    themeToggleLightIcon.classList.remove('hidden');
-} else {
-    themeToggleDarkIcon.classList.remove('hidden');
-}
+// Escucha el evento 'change' en el checkbox
+themeToggleCheckbox.addEventListener('change', function() {
+    // Toggle icons dentro del checkbox
+    /*themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');*/
 
-let themeToggleBtn = document.getElementById('theme-toggle');
-
-themeToggleBtn.addEventListener('click', function() {
-
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
-
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
-
-        // if NOT set via local storage previously
+    // Verifica el estado del checkbox
+    if (themeToggleCheckbox.checked) {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
     } else {
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+    }
+});
+
+// Verifica y aplica el tema almacenado en localStorage al cargar la página
+document.addEventListener('DOMContentLoaded', function() {
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'dark') {
             document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
+            themeToggleCheckbox.checked = true; // Marcar el checkbox si está en modo oscuro
+        } else {
+            document.documentElement.classList.remove('dark');
+            themeToggleCheckbox.checked = false; // Desmarcar el checkbox si está en modo claro
         }
     }
-
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ export default {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/views/welcome.blade.php',
         './node_modules/flowbite/**/*.js'
     ],
 
