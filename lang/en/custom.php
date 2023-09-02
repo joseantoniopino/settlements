7d0683a15290c2cbadebc81d7fb67b942bdc0f31
JoseA <josanpino@gmail.com>
2023-09-03 01:56:49 +0200

    feat(welcome layout): Grid card

--- lang/en/custom.php
+++ lang/en/custom.php
@@ -5,8 +5,21 @@ declare(strict_types=1);
 
 return [
     'settlement' => 'settlement',
-    'extraordinary_settlements' => 'Extraordinary settlements',
+    'extraordinary_settlements' => 'extraordinary settlements',
     'create_extraordinary_settlements' => 'Create extraordinary settlements',
     'hero_intro' => 'From exotic market stalls flourishing in unexplored corners, to age-old fortresses concealing mysteries and chambers forgotten by time',
-    'trading_posts' => 'trading posts'
+    'trading_posts' => 'trading posts',
+    'trading_post_card_description' => 'Trade posts are small settlements built in strategic locations to facilitate trade between different regions. They are often inhabited by traders and their families and offer a variety of services, such as accommodation, food, and stables for animals. Trade posts are typically bustling and lively places as they attract travelers from all corners.',
+    'villages' => 'villages',
+    'villages_card_description' => 'Villages are small rural communities with a population that generally does not exceed a few hundred inhabitants. These settlements play a crucial role in providing food and stability in the regions they belong to. It is common for a realm to have numerous villages scattered across its territories. In these places, generations of families build communities rooted in agriculture and the procurement of local resources.',
+    'towns' => 'towns',
+    'town_card_description' => 'Towns are more developed and larger places than villages and trade posts, with a population that typically reaches a few thousand inhabitants. These settlements are more structured, and it is uncommon for one of their residents to personally know everyone else. Towns combine the commercial aspects of trade posts with the community aspects of villages and often have their own local government.',
+    'cities' => 'cities',
+    'city_card_description' => 'Large cities, due to their size and scope, often expand extensively and feature specialized places such as academies, majestic temples, and government buildings. They also serve as cultural hubs and host numerous local events. Adventurers can find a variety of items for sale, increasing their chances of discovering rare treasures.',
+    'capitals' => 'capitals',
+    'capital_card_description' => 'Capitals are cities that house the seat of a nation\'s leader, such as a monarch. They are significant hubs with a substantial population, impressive buildings, and ample exploration opportunities. The population of a capital typically ranges from fifteen to twenty thousand residents, but in exceptional cases, it can exceed two hundred thousand or even a million, depending on the historical context.',
+    'fortresses' => 'fortresses',
+    'fortress_card_description' => 'Fortifications are sturdy and secure sites, typically of a defensive nature, such as forts, castles, or fortresses. They can be located in remote areas or urban zones and serve various purposes, from residences to organizational headquarters. The proximity to a fortification not only provides security but also suggests the presence of valuable resources in the region.',
+
+
 ];
--- lang/es/custom.php
+++ lang/es/custom.php
@@ -3,9 +3,22 @@
 declare(strict_types=1);
 
 return [
-    'settlement' => 'Asentamiento',
-    'extraordinary_settlements' => 'Asentamientos extraordinarios',
+    'settlement' => 'asentamiento',
+    'extraordinary_settlements' => 'asentamientos extraordinarios',
     'create_extraordinary_settlements' => 'Crea asentamientos extraordinarios',
     'hero_intro' => 'Desde exóticos puestos comerciales que florecen en rincones inexplorados, hasta milenarias fortalezas que ocultan misterios y cámaras olvidadas por el tiempo',
     'trading_posts' => 'puestos comerciales',
+    'trading_post_card_description' => 'Los puestos comerciales son pequeños asentamientos construidos en lugares estratégicos para facilitar el comercio entre diferentes regiones. Suelen estar habitados por comerciantes y sus familias, y ofrecen una variedad de servicios, como alojamiento, comida y establos para animales. Los puestos comerciales suelen ser lugares bulliciosos y llenos de vida, ya que atraen a viajeros de todos los rincones.',
+    'villages' => 'pueblos',
+    'villages_card_description' => 'Los pueblos son pequeñas comunidades rurales con una población que generalmente no supera las pocas centenas de habitantes. Estos asentamientos desempeñan un papel crucial al proporcionar alimentos y estabilidad en las regiones a las que pertenecen. Es común que un reino cuente con numerosos pueblos dispersos por sus territorios. En estos lugares, generaciones de familias construyen comunidades arraigadas en la agricultura y la obtención de recursos locales.',
+    'towns' => 'villas',
+    'town_card_description' => 'Las villas son lugares más desarrollados y de mayor tamaño que los pueblos y los puestos comerciales, con una población que suele alcanzar algunos miles de habitantes. Estos enclaves están más estructurados, y es poco común que uno de sus habitantes conozca personalmente a todos los demás. Las villas combinan los aspectos comerciales de los puestos comerciales con los aspectos comunitarios de los pueblos y a menudo tienen su propio gobierno local.',
+    'cities' => 'ciudades',
+    'city_card_description' => 'Las ciudades grandes, por su tamaño y alcance, suelen expandirse ampliamente y cuentan con lugares especializados como academias, templos majestuosos y edificios gubernamentales. Además, son centros culturales y hogar de numerosos eventos locales. Los aventureros pueden encontrar una variedad de objetos a la venta, lo que aumenta sus posibilidades de hallar raros tesoros.',
+    'capitals' => 'capitales',
+    'capital_card_description' => 'Las capitales son ciudades que albergan la sede del líder de una nación, como un monarca. Son centros importantes con una población significativa, impresionantes edificios y muchas oportunidades de exploración. La población de una capital suele variar entre quince mil y veinte mil habitantes, pero en casos excepcionales, puede superar los doscientos mil o incluso el millón, dependiendo del contexto histórico.',
+    'fortresses' => 'fortificaciones',
+    'fortress_card_description' => 'Las fortificaciones son enclaves sólidos y seguros, generalmente de carácter defensivo, como fuertes, castillos o fortalezas. Pueden estar ubicadas en lugares remotos o en zonas urbanas y desempeñan diversas funciones, desde residencias hasta sedes de organizaciones. La proximidad a una fortificación no solo brinda seguridad, sino que también indica la presencia de recursos valiosos en la región.',
+
+
 ];
